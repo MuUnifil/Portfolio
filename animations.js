1e2e74e800222277978ed901ac1bb436cb6180bd
Murilo Haouli <murilo.haouli@edu.unifil.br>
2024-10-29 14:18:34 -0300

    Add files via upload

---
+++ animations.js
@@ -0,0 +1,11 @@
+//MUDAR BACKGROUND QUANDO PASSAR O MOUSE(opacidade)
+
+//Tema Claro/Escuro
+
+/*TER UMA SEÇÃO PARA CERTIFICADOS ANIMADA
+Exibição de Certificados: Um carrossel ou galeria interativa que exibe certificações (com links para as versões originais).*/
+
+/*Formulário de Contato
+Formulário com Validação em Tempo Real: Um formulário de contato onde as validações acontecem enquanto o usuário preenche (como avisos se o e-mail está no formato correto).
+
+Envio de Mensagem via API: Integrar com um serviço de envio de e-mails usando, por exemplo, EmailJS, para que as mensagens possam ser enviadas diretamente do site sem recarregar a página.*/
