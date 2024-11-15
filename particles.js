646ee9de3ad4bdc773e93db56ec2d50f7eb82d1d
Murilo Haouli <murilo.haouli@edu.unifil.br>
2024-11-14 23:41:25 -0300

    Update particles.js

--- particles.js
+++ particles.js
@@ -37,14 +37,14 @@ particlesJS("particles-js", {
         }
     },
     interactivity: {
-        detect_on: "canvas",
+        detect_on: "window",
         events: {
             onhover: { enable: true, mode: "grab" },
             onclick: { enable: true, mode: "push" },
             resize: true
         },
         modes: {
-            grab: { distance: 225, line_linked: { opacity: 1 } },
+            grab: { distance: 200, line_linked: { opacity: 1 } },
             bubble: { distance: 400, size: 40, duration: 2, opacity: 8, speed: 3 },
             repulse: { distance: 200, duration: 0.4 },
             push: { particles_nb: 4 },
@@ -53,20 +53,3 @@ particlesJS("particles-js", {
     },
     retina_detect: true
 });
-var count_particles, stats, update;
-stats = new Stats();
-stats.setMode(0);
-stats.domElement.style.position = "absolute";
-stats.domElement.style.left = "0px";
-stats.domElement.style.top = "0px";
-document.body.appendChild(stats.domElement);
-count_particles = document.querySelector(".js-count-particles");
-update = function() {
-    stats.begin();
-    stats.end();
-    if (window.pJSDom[0].pJS.particles && window.pJSDom[0].pJS.particles.array) {
-        count_particles.innerText = window.pJSDom[0].pJS.particles.array.length;
-    }
-    requestAnimationFrame(update);
-};
-requestAnimationFrame(update);
