e2156f601626f8c49abbdf1aa9ff99d68a670cce
Murilo Haouli <murilo.haouli@edu.unifil.br>
2024-11-23 18:33:16 -0300

    Update changePage.js

--- changePage.js
+++ changePage.js
@@ -1,19 +1,21 @@
 document.addEventListener('DOMContentLoaded', function () {
     const body = document.body;
-    const elementsToAnimate = document.querySelectorAll('.fade-in'); // Seleciona os elementos
-
-    // Adiciona a classe "visible" com atraso para cada elemento
-    elementsToAnimate.forEach((element, index) => {
-        setTimeout(() => {
-            element.classList.add('visible');
-        }, index * 200); // Incrementa 200ms para cada elemento
-    });
-
-    // Remove a transição ao trocar de página
-    body.style.opacity = 1;
-
-    const navLinks = document.querySelectorAll('.lista-navegacao a');
+    const elementsToAnimate = document.querySelectorAll('.fade-in');
     const loader = document.querySelector('.loader');
+
+    // Função para adicionar animações de entrada
+    function animateElements() {
+        elementsToAnimate.forEach((element, index) => {
+            setTimeout(() => {
+                element.classList.add('visible');
+            }, index * 200);
+        });
+    }
+
+    // Inicializa animações ao carregar a página
+    animateElements();
+
+    const navLinks = document.querySelectorAll('nav a');
 
     function fadeOut(currentUrl) {
         body.style.transition = 'opacity 0.5s ease';
@@ -21,14 +23,23 @@ document.addEventListener('DOMContentLoaded', function () {
         loader.style.transform = 'translateX(0%)';
 
         setTimeout(() => {
-            window.location.href = currentUrl;
+            if (currentUrl === window.location.href) {
+                // Força o reinício da página se for o mesmo URL
+                body.style.opacity = 1;
+                loader.style.transform = 'translateX(-100%)';
+                elementsToAnimate.forEach(element => element.classList.remove('visible')); // Remove a classe 'visible'
+                animateElements(); // Reinicia as animações
+            } else {
+                window.location.href = currentUrl;
+            }
         }, 500);
     }
 
     navLinks.forEach(link => {
         link.addEventListener('click', function (e) {
             e.preventDefault();
-            const url = this.getAttribute('href');
+            const url = this.href;
+
             fadeOut(url);
         });
     });
